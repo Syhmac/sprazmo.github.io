f826eeb15a0762dfc6a7383b7a0dc12fefc44988
Samuel Praźmo <syhmac@syhmac.pl>
2026-04-09 10:06:36 +0200

    Lab-c

--- lab-c/scripts.js
+++ lab-c/scripts.js
@@ -1,1 +1,11 @@
-console.debug("Hello world!");
+let map = L.map('map').setView([53.432780, 14.548085], 16);
+L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
+
+document.getElementById("export").addEventListener("click", () => {
+  leafletImage(map, (err, canvas) => {
+    let rastMap = document.getElementById("puzzleBox");
+    let rastCtx = rastMap.getContext("2d")
+
+    rastCtx.drawImage(canvas, 0, 0, rastMap.width, rastMap.height);
+  })
+})
