6bb904159148b6497cc57242a6fdc69e72a86702
Samuel Praźmo <syhmac@syhmac.pl>
2026-04-16 09:41:21 +0200

    Lab-c - puzzle generation and shuffle

--- lab-c/scripts.js
+++ lab-c/scripts.js
@@ -1,11 +1,70 @@
 let map = L.map('map').setView([53.432780, 14.548085], 16);
 L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
 
+function shuffle(array) {
+  let currIdx = array.length;
+
+  while (currIdx !== 0) {
+    let randIdx = Math.floor(Math.random() * currIdx);
+    currIdx--;
+
+    [array[currIdx], array[randIdx]] = [array[randIdx], array[currIdx]];
+  }
+}
+
 document.getElementById("export").addEventListener("click", () => {
   leafletImage(map, (err, canvas) => {
     let rastMap = document.getElementById("puzzleBox");
     let rastCtx = rastMap.getContext("2d")
 
+    const puzzleHeight = rastMap.height / 4;
+    const puzzleWidth = rastMap.width / 4;
+    const puzzleImage = canvas.toDataURL("image/png");
+    let puzzles = [];
+    let puzzlesBox = document.getElementById("puzzles");
+
     rastCtx.drawImage(canvas, 0, 0, rastMap.width, rastMap.height);
+
+    let puzzleIdx = 1
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        // Create puzzle element
+        let puzzle = document.createElement("canvas");
+        puzzle.classList.add("puzzle");
+        puzzle.width = puzzleWidth;
+        puzzle.height = puzzleHeight;
+        puzzle.setAttribute("puzzleID", puzzleIdx++)
+        // Add puzzle to array
+        puzzles.push(puzzle);
+        // Draw puzzle piece
+        let ctx = puzzle.getContext("2d");
+        ctx.drawImage(rastMap, j * puzzleWidth, i * puzzleHeight, puzzleWidth, puzzleHeight, 0, 0, puzzleWidth, puzzleHeight);
+        // TODO: Add drag-and-drop functionality
+      }
+    }
+
+    shuffle(puzzles);
+
+    for (let puzzle of puzzles) {
+      puzzlesBox.appendChild(puzzle);
+    }
+
+  })
+})
+
+document.getElementById("myLocation").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    alert("Geolocation is not supported by your browser");
+    return;
+  }
+  navigator.geolocation.getCurrentPosition((pos) => {
+    const lat = pos.coords.latitude;
+    const lng = pos.coords.longitude;
+    map.setView([lat, lng], 16);
+  }, (err) => {
+    console.error(err);
+  }, {
+    enableHighAccuracy: true,
   })
 })
