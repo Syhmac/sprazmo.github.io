73120cb25984d287ebedbe9ac6434e4d4792193d
Samuel Praźmo <syhmac@syhmac.pl>
2026-04-27 13:59:16 +0200

    Lab-d - responsive layout

---
+++ lab-d/layout_test.js
@@ -0,0 +1,10 @@
+function layout_test() {
+  let parent = document.getElementById("forecastWeather")
+  let child = parent.querySelector(".forecastItem")
+  for (let i = 0; i < 10; i++) {
+    let child_copy = child.cloneNode(true)
+    parent.appendChild(child_copy)
+  }
+}
+
+layout_test()
